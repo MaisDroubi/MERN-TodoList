0fe74c3afc7a58875ebd44ce7665f7c11543f9ff
MaisDroubi <mais.droubi@gmail.com>
2021-02-03 20:47:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import ToDo from './components/ToDo';
+
+function App() {
+   return (
+    <div className="App">
+     <ToDo></ToDo>
+    </div>
+  );
+}
+
+export default App;
